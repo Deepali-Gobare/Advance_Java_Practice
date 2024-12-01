b5704a8ad62842bab06fa6ef4da45e40e548b2f6
Deeplai-Gobare <deepaligobar07@gmail.com>
2024-12-02 01:49:05 +0530

    Home Practice

---
+++ Spring/Spring_Demo/src/main/java/com/demo/beans/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.demo.beans;
+
+
+
+public class HelloWorld {
+	public void sayHello() {
+		System.out.println("Hello world");
+	}
+
+	public HelloWorld() {
+		System.out.println("Destructor of hello world");
+
+	}
+	
+	
+
+}
